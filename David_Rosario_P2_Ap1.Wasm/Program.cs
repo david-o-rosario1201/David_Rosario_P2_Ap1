c4057232ed5b9b3bc85ed3fa98a90a3d01838efe
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-02 13:38:40 -0400

    Add project files.

---
+++ David_Rosario_P2_Ap1.Wasm/Program.cs
@@ -0,0 +1,11 @@
+using David_Rosario_P2_Ap1.Wasm;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5070") });
+
+await builder.Build().RunAsync();

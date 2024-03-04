b2515946ac2cf5fa5fc2fffdc68511d38ca80e1a
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-04 19:19:23 -0400

    Hice el modelo VehiculosDetalle

---
+++ Library/Models/VehiculosDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models;
+
+public class VehiculosDetalle
+{
+	[Key]
+	public int Id { get; set; }
+
+    public int VehiculoId { get; set; }
+
+    public int AccesorioId { get; set; }
+
+    public decimal Valor { get; set; }
+}

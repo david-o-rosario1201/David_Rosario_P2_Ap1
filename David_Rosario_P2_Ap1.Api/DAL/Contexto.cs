ff3431a7b8ad6999872dc72ad8404ce1f85f115d
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-04 18:50:53 -0400

    Hice el modelo Accesorios, lo agregué al Contexto e hice un OnModelCreating.

--- David_Rosario_P2_Ap1.Api/DAL/Contexto.cs
+++ David_Rosario_P2_Ap1.Api/DAL/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace David_Rosario_P2_Ap1.Api.DAL;
 
@@ -8,4 +9,21 @@ public class Contexto : DbContext
     {
         
     }
+
+    public DbSet<Accesorios> Accesorios { get; set; }
+
+
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Accesorios>().HasData(new List<Accesorios>()
+        {
+            new Accesorios() { AccesorioId = 1, Descripcion = "Camara Trasera"},
+            new Accesorios() { AccesorioId = 2, Descripcion = "Pantalla Interior"},
+            new Accesorios() { AccesorioId = 3, Descripcion = "Interior en Piel"},
+            new Accesorios() { AccesorioId = 4, Descripcion = "Sun Roof"},
+            new Accesorios() { AccesorioId = 5, Descripcion = "Aros de Lujo"}
+        });
+	}
 }
